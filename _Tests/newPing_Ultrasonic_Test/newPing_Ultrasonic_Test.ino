e1b18b49a17675f56c62f223b6b4f6b28ff57e08
Bridget <51479040+bridget512@users.noreply.github.com>
2020-04-16 16:48:25 +1000

    Update newPing_Ultrasonic_Test.ino

--- _Tests/newPing_Ultrasonic_Test/newPing_Ultrasonic_Test.ino
+++ _Tests/newPing_Ultrasonic_Test/newPing_Ultrasonic_Test.ino
@@ -5,13 +5,7 @@
 #define echo01 7
 #define maxDistance 200
 
-// Running two at once test
-// #define trig02 8
-// #define echo02 8
-
-
 NewPing sensor01(trig01, echo01, maxDistance);
-//NewPing sensor02(trig02, echo02, maxDistance);
 
 void setup() {
   Serial.begin(9600);
@@ -20,6 +14,4 @@ void setup() {
 void loop() {
   delay(50);
   Serial.println(sensor01.ping_cm()); // Gets output from sensor 1
-//   delay(50);
-//   Serial.println(sensor02.ping_cm()); // Gets output from sensor 2
 }

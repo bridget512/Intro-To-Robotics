ccd30a10c109c92cc4b5be7f773bce9b7227b096
Bridget <51479040+bridget512@users.noreply.github.com>
2020-04-16 16:47:33 +1000

    Update newPing_Ultrasonic_Test.ino

--- _Tests/newPing_Ultrasonic_Test/newPing_Ultrasonic_Test.ino
+++ _Tests/newPing_Ultrasonic_Test/newPing_Ultrasonic_Test.ino
@@ -1,14 +1,17 @@
 #include <NewPing.h>
 
+// Yes, they're both on pin 7. Not a typo!
 #define trig01 7
 #define echo01 7
-
-#define trig02 8
-#define echo02 8
 #define maxDistance 200
 
+// Running two at once test
+// #define trig02 8
+// #define echo02 8
+
+
 NewPing sensor01(trig01, echo01, maxDistance);
-NewPing sensor02(trig02, echo02, maxDistance);
+//NewPing sensor02(trig02, echo02, maxDistance);
 
 void setup() {
   Serial.begin(9600);
@@ -16,7 +19,7 @@ void setup() {
 
 void loop() {
   delay(50);
-  Serial.println(sensor01.ping_cm());
-  delay(50);
-  Serial.println(sensor02.ping_cm());
+  Serial.println(sensor01.ping_cm()); // Gets output from sensor 1
+//   delay(50);
+//   Serial.println(sensor02.ping_cm()); // Gets output from sensor 2
 }

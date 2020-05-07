201007e8075d496d99d8a3681086f7da7d84af51
Bridget <51479040+bridget512@users.noreply.github.com>
2020-05-07 10:33:54 +1000

    Update Roboi.ino

--- Roboi.ino
+++ Roboi.ino
@@ -2,13 +2,13 @@ int debug_showsensorFrontDistance = true; // Output distance values to SerialMon
 
 //Motor Controller
 #include <SparkFun_TB6612.h> //manual download, not in library manager
-#define PWMA 11
-#define AIN2 10
-#define AIN1 9
-#define STBY 8
-#define BIN1 3
-#define BIN2 5
-#define PWMB 6
+#define PWMA 12
+#define AIN2 11
+#define AIN1 10
+#define STBY 13
+#define BIN1 9
+#define BIN2 8
+#define PWMB 7
 
 const int offsetA = -1;
 const int offsetB = -1;
@@ -16,17 +16,17 @@ const int offsetB = -1;
 Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
 Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
 
-int motorSpeed = 255;
+int motorSpeed = 180;
 
 // Servo 180d (SG90) Reference code from http://www.educ8s.tv. Modified for project.
 #include <Servo.h>
 Servo myservo;   
-int servoPin = 13;
+int servoPin = 2;
 int servoRange = 180; // Total rotation of servo. SG90
-int servoCenter = 80;//servoRange / 2; 
+int servoCenter = 85; 
 int servoEdgeOffset = 0; 
-int servoRangeMin = servoRange - servoRange + servoEdgeOffset;
-int servoRangeMax = servoRange - servoEdgeOffset;
+int servoRangeMin = servoRange - servoRange + servoEdgeOffset + 5;
+int servoRangeMax = servoRange - servoEdgeOffset + 5;
 
 boolean isMovingForward = false;
 int distance = 100;
@@ -35,21 +35,45 @@ int scanDistance;
 // HC-SR04 (NewPing Library Single Pin Mode)
 #include <NewPing.h>
 #define maxDistance 350
-#define sensorFrontPin 2 // Shared pin for Trigger and Echo
+#define sensorFrontPin 3 // Shared pin for Trigger and Echo
 
 NewPing sensorFrontPing(sensorFrontPin, sensorFrontPin, maxDistance);
 
 int sensorFront;
 int sensorFrontDistance; // Final output passed to the loop
 
+// Dot Matrix Display
+#include <LedControl.h>
+
+int DIN = 26;
+int CS =  28;
+int CLK = 30;
+
+byte happy[8]= {0x4,0x2,0x24,0x40,0x40,0x24,0x2,0x4};
+byte neutral[8]= {0x0,0x0,0x2c,0x40,0x40,0x2c,0x0,0x0};
+byte surprise[8]= {0x0,0x0,0x4c,0xa0,0xa0,0x4c,0x0,0x0};
+byte sad[8]= {0x0,0x0,0x4c,0x20,0x20,0x4c,0x0,0x0};
+
+LedControl lc=LedControl(DIN,CLK,CS,1);
+
+//#include <SoftwareSerial.h>
+//char BTcommand;
+//int RX = 42;
+//int TX = 40;
+// 
+//SoftwareSerial BTserial(RX, TX);
+
 //========================================================== Setup()
 
 void setup(){ 
-
+  
   Serial.begin(9600);
-
   myservo.attach(servoPin);  
   myservo.write(servoCenter); 
+
+  lc.shutdown(0,false);       //The MAX72XX is in power-saving mode on startup
+  lc.setIntensity(0,1);      // Set the brightness to maximum value
+  lc.clearDisplay(0);         // and clear the display
   
 } //void setup
 
@@ -57,14 +81,16 @@ void setup(){
 
 void loop() {
 
-  sensorFrontDistance = sensor(sensorFront);
+ sensorFrontDistance = sensorFrontPing.ping_cm(); ;
   
   if(debug_showsensorFrontDistance == true){Serial.println(sensorFrontDistance);}
+
 
   int distanceR = 0;
   int distanceL = 0;
   delay(40);
- 
+
+    
  if(sensorFrontDistance <= 25){
   brake();
   delay(100);
@@ -76,6 +102,10 @@ void loop() {
   delay(200);
   distanceL = scanLeft();
   delay(200);
+
+
+  
+    
   
   if(distanceR >= distanceL){
     right();
@@ -91,6 +121,9 @@ void loop() {
   else{
     forward();
   }
+
+
+
   
   sensorFrontDistance;
 
@@ -103,6 +136,7 @@ void forward(){
     isMovingForward = true;
     forward(motor1, motor2, motorSpeed);
     Serial.println("Forward!");
+    printByte(happy);
   }
 }
 
@@ -110,6 +144,7 @@ void reverse(){
   isMovingForward = false;
   forward(motor1, motor2, -motorSpeed);
   Serial.println("Reverse!");
+  printByte(sad);
 }
 
 void right(){
@@ -117,6 +152,7 @@ void right(){
   motor2.drive(-motorSpeed);
   delay(300);
   Serial.println("Turning Right!");
+  printByte(surprise);
 }
 
 void left(){
@@ -124,17 +160,20 @@ void left(){
   motor2.drive(motorSpeed);
   delay(300);
   Serial.println("Turning Left!");
+  printByte(surprise);
 }
 
 void brake(){
+  
   brake(motor1, motor2);
   Serial.println("Braking!");
+  printByte(neutral);
 }
 
 //========================================================== Sensor and Scanning
 
 int sensor(int sensorFront){
-  delay(70);
+  delay(50);
   sensorFront = sensorFrontPing.ping_cm(); 
   if(sensorFront == 0){
     sensorFront = 250;
@@ -145,9 +184,9 @@ int sensor(int sensorFront){
 int scanRight(){
   Serial.println("Scanning Right");
   myservo.write(servoRangeMin);
-  delay(500);
-  int scanDistance = sensor(sensorFront) ;
   delay(100);
+  int scanDistance = sensor(sensorFront) ;
+  //delay(50);
   myservo.write(servoCenter);
   Serial.println(scanDistance);
   return scanDistance;
@@ -156,12 +195,55 @@ int scanRight(){
 int scanLeft(){
   Serial.println("Scanning Left");
   myservo.write(servoRangeMax);
-  delay(500);
-  int scanDistance = sensor(sensorFront);
   delay(100);
+  int scanDistance = sensor(sensorFront);
+  //delay(50);
   myservo.write(servoCenter);
   Serial.println(scanDistance);
   return scanDistance;
 }
 
+//========================================================== Face
+void printByte(byte character []){
+  int i = 0;
+  
+  for(i=0;i<8;i++){
+    lc.setRow(0,i,character[i]);
+  }
+}
 
+//========================================================== Bluetooth - not working yet
+
+//void bluetooth(){
+//  while (!BTserial.available());
+//  BTcommand = BTserial.read();
+//
+//  if(BTcommand == '1'){
+//    brake();
+//    forward();
+//  }
+//  
+//  else if(BTcommand == '2'){
+//    brake();
+//  }
+//  
+//  else if(BTcommand == '3'){
+//    brake();
+//    delay(250);
+//    left();
+//    delay(300);
+//    brake();
+//  }
+//  
+//  else if(BTcommand == '4'){
+//    brake();
+//    delay(250);
+//    right();
+//    delay(300);
+//    brake();
+//  }
+//  
+//  else{
+//    brake();
+//  }
+//}

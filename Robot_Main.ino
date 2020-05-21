150a8bdfc44a8fe5d4177bb3ecb310a0f9a63c0f
Bridget <51479040+bridget512@users.noreply.github.com>
2020-05-21 13:04:01 +1000

    Update Robot_Main.ino

--- Robot_Main.ino
+++ Robot_Main.ino
@@ -4,9 +4,10 @@
 //==========================================================
 // Pin Setup for Mega2560
 
-int debug = true; // Output distance values to Serial Monitor
-int enable_sweep = false; // Enable/Disable front servo to sweep and take measurements while moving
+int debug = false; // Output distance values to Serial Monitor
 int enable_motor = true; // Enable/Disable Motor Driver
+int enable_frontCornerObstacleDetection = true;
+int enable_boredomDetection = true;
 
 //========================================================== Motor
 //Motor Controller
@@ -25,7 +26,7 @@ const int offsetB = -1;
 Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
 Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
 
-int motorSpeed = 180;
+int motorSpeed = 170;
 
 //========================================================== Servo
 // Servo 180d (SG90) Reference code from http://www.educ8s.tv. Modified for project.
@@ -43,6 +44,7 @@ int distance = 100;
 int scanDistance;
 
 //========================================================== HC-SR04 (NewPing Library Single Pin Mode)
+// Base code and library from https://github.com/microflo/NewPing/blob/master/NewPing.h
 #include <NewPing.h>
 #define maxDistance 250
 #define sensorFrontPin 41 // Shared pin for Trigger and Echo
@@ -52,11 +54,6 @@ NewPing sensorFrontPing(sensorFrontPin, sensorFrontPin, maxDistance);
 int sensorFront;
 int sensorFrontDistance; // Final output passed to the loop
 
-int sensorFrontRight;
-int sensorFrontRightDistance; // Final output passed to the loop
-int sensorFrontLeft;
-int sensorFrontLeftDistance; // Final output passed to the loop
-
 //========================================================== IR Distance Sensors
 int irLeftPin = 3;
 int irRightPin = 4;
@@ -64,6 +61,7 @@ int irLeft = HIGH;
 int irRight = HIGH;
 
 //========================================================== Dot Matrix Display
+// Base code from http://reefwingrobotics.blogspot.com/2017/11/duinotech-8-x-8-led-dot-matrix-module.html
 #include <LedControl.h>
 
 int DIN = 22;
@@ -77,27 +75,26 @@ byte matrix_sad[8]= {0x0,0x0,0x4c,0x20,0x20,0x4c,0x0,0x0};
 byte matrix_left[8]= {0x0,0x0,0x0,0x4c,0x20,0x20,0x4c,0x0};
 byte matrix_right[8]= {0x0,0x4c,0x20,0x20,0x4c,0x0,0x0,0x0};
 byte matrix_angry[8]= {0x01,0x06,0x44,0x20,0x20,0x44,0x06,0x01};
+byte matrix_heart[8] = {0x0C,0x1E,0x3E,0x7C,0x7C,0x3E,0x1E,0x0C};
 
 LedControl lc=LedControl(DIN,CLK,CS,1);
 
 //========================================================== Capacitive Touch Button
+// Simple button
 int touchButtonPin = 32;
-int touchButtonState = LOW;
-
+int touchButtonState = 0;
+int touchButtonLastState = 0;
 //========================================================== Piezo Speaker I2C to Arduino
+// Wire usage from Arduino Playground
 #include <Wire.h>
 #define SLAVE_ADDR 9
 int playSoundNumber;
 
-//========================================================== Bluetooth
-//#include <SoftwareSerial.h>
-////char BTcommand;
-//int RX = 50;
-//int TX = 52;
-// 
-//SoftwareSerial BTserial(RX, TX);
-//char BTcommand;
 
+long currentMillis = 0;
+long previousMillis = 0;
+
+int isMovingForwardLast = 0;
 //========================================================== Setup()
 void setup(){ 
   Wire.begin();
@@ -128,10 +125,9 @@ void setup(){
 void loop() {
   if(debug == true){Serial.println(sensorFrontDistance);}
   if(enable_motor == true){digitalWrite(STBY, HIGH);} else{digitalWrite(STBY, LOW);}
+  if(enable_boredomDetection == true){boredomDetection();}
+  if(enable_frontCornerObstacleDetection == true){ir_frontCorners();}
 
-//  while (!BTserial.available());
-//  BTcommand = BTserial.read();
-//  Serial.println(BTcommand);
   Wire.beginTransmission(SLAVE_ADDR);
   Wire.write(playSoundNumber);
   Wire.endTransmission();
@@ -144,6 +140,7 @@ void loop() {
   int distanceL = 0;
   delay(40);
   int choice;
+  int choice2;
   int proxMin = 20;
 
 // If the robot comes to a wall, turn servo and get the distance. Largest distance wins.
@@ -171,19 +168,12 @@ void loop() {
 
 }
 
-// Runs the code for the IR sensors. Used as object avoidance
-ir_frontCorners();
-
 // If the robot scans left and right and the distances are less than the minimum proximitiy distance, turn around
 //========================================================== Dead End
   if(sensorFrontDistance >= 1 && sensorFrontDistance <= proxMin && 
-     distanceR <= proxMin && distanceL <= proxMin){
+     distanceR <= proxMin && distanceL <= proxMin + 20){
       
-    playSoundNumber = 3;
-    
-    Wire.beginTransmission(SLAVE_ADDR);
-    Wire.write(playSoundNumber);
-    Wire.endTransmission();
+    playSoundNumber = 3; Wire.beginTransmission(SLAVE_ADDR); Wire.write(playSoundNumber); Wire.endTransmission();
     
     Serial.println("Action: Dead End! Turning around!");
     choice = random(1, 128);
@@ -211,24 +201,27 @@ ir_frontCorners();
 
 //========================================================== Touch Sensor
 
-// If the sensor is touched while moving, the robot will stop and be matrix_sad
+// If the sensor is touched while moving, the robot will stop and look at the button
 if(isMovingForward == true && touchButtonState == HIGH){
   Serial.println("Touch: You booped me!");
-  playSoundNumber = 2;
-  Serial.println(playSoundNumber);
+  playSoundNumber = 2; Wire.beginTransmission(SLAVE_ADDR); Wire.write(playSoundNumber); Wire.endTransmission();
   isMovingForward = false;
-  printByte(matrix_sad);
+  printByte(matrix_left);
+  myservo.write(servoRangeMax);
+  delay(150);
+  myservo.write(servoCenter);
   brake();
   delay(1000);
   forward();
+
 }
 
 // This is the default forward for the loop, don't move me!
 else{
   forward();
+  
   playSoundNumber = 0;
 }
-  
   sensorFrontDistance;
 
 }//====================================================================== END Loop()
@@ -240,11 +233,14 @@ void forward(){
   if(!isMovingForward){
     isMovingForward = true;
     forward(motor1, motor2, motorSpeed);
+    previousMillis = currentMillis;
     Serial.println("Action: Moving Forward!");
   }
 }
 
 void reverse(){
+  playSoundNumber = 4;
+  Wire.beginTransmission(SLAVE_ADDR); Wire.write(playSoundNumber); Wire.endTransmission();
   isMovingForward = false;
   forward(motor1, motor2, -motorSpeed);
   Serial.println("Action: Reverse!");
@@ -254,25 +250,23 @@ void reverse(){
 void right(){
   motor1.drive(motorSpeed);
   motor2.drive(-motorSpeed);
-  delay(250);
+  delay(280);
   Serial.println("Action: Turning Right!");
 }
 
 void left(){
   motor1.drive(-motorSpeed);
   motor2.drive(motorSpeed);
-  delay(250);
+  delay(280);
   Serial.println("Action: Turning Left!");
 }
 
 void brake(){
   brake(motor1, motor2);
   Serial.println("Action: Braking!");
-  //printByte(matrix_neutral);
 }
 
 //========================================================== Sensor and Scanning
-
 int sensor(int sensorFront){
   delay(50);
   sensorFront = sensorFrontPing.ping_cm(); 
@@ -286,7 +280,7 @@ int scanRight90(){
   Serial.println("Vision: Scanning Right");
   printByte(matrix_right);
   myservo.write(servoRangeMin);
-  delay(100);
+  delay(180);
   int scanDistance = sensor(sensorFront) ;
   delay(50);
   myservo.write(servoCenter);
@@ -298,7 +292,7 @@ int scanLeft90(){
   Serial.println("Vision: Scanning Left");
   printByte(matrix_left);
   myservo.write(servoRangeMax);
-  delay(100);
+  delay(180);
   int scanDistance = sensor(sensorFront);
   delay(50);
   myservo.write(servoCenter);
@@ -323,25 +317,94 @@ void ir_frontCorners(){
 
   if(isMovingForward == true && irLeft == LOW){
     printByte(matrix_left);
+    playSoundNumber = 5;
+    Wire.beginTransmission(SLAVE_ADDR); Wire.write(playSoundNumber); Wire.endTransmission();
     Serial.println("Too close on left ir_frontCorners!");
     isMovingForward = false;
-    motor1.drive(motorSpeed - 80);
-    motor2.drive(-motorSpeed +80);
+    motor1.drive(motorSpeed - 70);
+    motor2.drive(-motorSpeed +70);
     delay(200);
   }
 
   if(isMovingForward == true && irRight == LOW){
     printByte(matrix_right);
+    playSoundNumber = 5;
+    Wire.beginTransmission(SLAVE_ADDR); Wire.write(playSoundNumber); Wire.endTransmission();
     Serial.println("Too close on right ir_frontCorners!");
     isMovingForward = false;
-    motor1.drive(-motorSpeed + 80);
-    motor2.drive(motorSpeed - 80);
+    motor1.drive(-motorSpeed + 70);
+    motor2.drive(motorSpeed - 70);
     delay(200);
     
   }
 }
 
-//========================================================== Bluetooth - not working yet, probably wont
-//void bluetooth(){
-//  while (!BTserial.available());
-//  BTcommand = BTserial.read();
+
+void boredomDetection(){
+
+  currentMillis = millis();
+
+  if(isMovingForward == HIGH && currentMillis - previousMillis > 4000){
+    isMovingForward = false;
+
+      previousMillis = currentMillis;
+     
+      myservo.write(servoRangeMin + 40);
+      delay(150);
+      myservo.write(servoRangeMax + 40);
+      delay(150);
+      myservo.write(servoCenter);
+      delay(150);
+      
+      int choice2;
+      choice2 = random(98);
+      
+      if(choice2 <= 32){
+        // Left
+        playSoundNumber = 2; Wire.beginTransmission(SLAVE_ADDR); Wire.write(playSoundNumber); Wire.endTransmission();
+        printByte(matrix_heart);
+        myservo.write(servoRangeMax + 40);
+        brake();
+        delay(150);
+        motor1.drive(-250, 750);
+        motor2.drive(250, 750);
+        Serial.println("Event: WWWwwwweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+        motor1.drive(0, 50);
+        motor2.drive(0, 50);
+        brake();
+        myservo.write(servoCenter);
+        delay(500);
+      }
+      
+      else if(choice2 > 32 && choice2 <= 65){
+        // Right
+        playSoundNumber = 2; Wire.beginTransmission(SLAVE_ADDR); Wire.write(playSoundNumber); Wire.endTransmission();
+        printByte(matrix_heart);
+        myservo.write(servoRangeMin + 40);
+        brake();
+        delay(150);
+        motor1.drive(250,500);
+        motor2.drive(-250,500);
+        Serial.println("Event: WWWwwwwoooooooooooooooooooooooooooooooooooooooo");
+        motor1.drive(0, 50);
+        motor2.drive(0, 50);
+        brake();
+        myservo.write(servoCenter);
+        delay(500);
+      }
+
+      else{
+        playSoundNumber = 4; Wire.beginTransmission(SLAVE_ADDR); Wire.write(playSoundNumber); Wire.endTransmission();
+        printByte(matrix_angry);
+        Serial.println("Event: Hmph");delay(500);
+        right();
+        right();
+        brake();
+        delay(50);     
+      }
+
+    }
+
+  isMovingForwardLast = isMovingForward;
+  //Serial.println(isMovingForward);
+}
